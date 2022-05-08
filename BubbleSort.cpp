ed7bfdc74f39341ebf1e606420fffea4f23a5418
Ben Cyna <benjamincyna@gmail.com>
2022-05-08 14:53:50 +0930

    added bubble sort functionality

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -3,5 +3,15 @@
 using namespace std;
 
 vector<int> BubbleSort::sort(std::vector<int> list) {
-    return list;
+    int length = list.size();
+    for (int i = 0; i < length; i++) {
+        for (int j = 0; j < length; j++) {
+            if (list.at(j) > list.at(j + 1)) {
+                // swap
+                int temp = list.at(j);
+                list.at(j) = list.at(j + 1);
+                list.at(j+1) = temp;
+            }
+        }
+    }
 }
