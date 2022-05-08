40e6889f1d33fa50eb7cec169225a358a7fe2c95
Ben Cyna <benjamincyna@gmail.com>
2022-05-08 17:43:23 +0930

    made main function able to take in input

---
+++ .vscode/test.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include "Sort.h"
+#include "BubbleSort.h"
+#include "QuickSort.h"
+#include "RecursiveBinarySearch.h"
+
+#include <vector>
+
+using namespace std;
+
+int main() {
+    // BUBBLE SORT
+
+    Sort * sort1;
+
+    BubbleSort * bubbleSort = new BubbleSort();
+
+    sort1 = bubbleSort;
+
+    vector<int> list1 = {1, 2, 3 , 4};
+    vector<int> list = sort1->sort(list1);
+
+    for (int i = 0; i < list.size(); i++) {
+        cout << list[i] << endl;
+    }
+
+    return 0; 
+
+    // ////////////////////////////////////
+// quick sort
+    Sort * sort1;
+
+    QuickSort * quickSort = new QuickSort();
+
+    sort1 = quickSort;
+
+    vector<int> list1 = {8, 3, 9, 438, 322, 23, 3};
+    vector<int> list = sort1->sort(list1);
+
+    for (int i = 0; i < list.size(); i++) {
+        cout << list[i] << endl;
+    }
+
+    ///////////////////////////////////
+
+    // Binary search
+
+        
+    RecursiveBinarySearch binarySearch;
+
+    vector<int> searchV = {1, 2 ,3 ,4, 5};
+
+    if (binarySearch.search(searchV, 6)) {
+        cout << "the item is in the vector" << endl;
+    }
+    else {
+        cout << "the item is not in the vector" << endl;
+    }
+}
--- main.cpp
+++ main.cpp
@@ -5,22 +5,46 @@
 #include "RecursiveBinarySearch.h"
 
 #include <vector>
+#include <string>
 
 using namespace std;
 
 int main() {
-    Sort * sort1;
 
-    QuickSort * quickSort = new QuickSort();
+    vector<int> integers;
+    string line;
 
-    sort1 = quickSort;
+    getline(cin, line);
 
-    vector<int> list1 = {8, 3, 9, 438, 322, 23, 3};
-    vector<int> list = sort1->sort(list1);
-
-    for (int i = 0; i < list.size(); i++) {
-        cout << list[i] << endl;
+    for (int i = 0; i < line.length(); i++) {
+        if (line.at(i) != ' ') {
+            int number = stoi(string(1, line.at(i)));
+            integers.push_back(number);
+        }
     }
+
+    Sort *sort;
+
+    QuickSort *quickSort = new QuickSort();
+
+    sort = quickSort;
+
+    vector<int> sortedIntegers = sort->sort(integers);
+
+    RecursiveBinarySearch binarySearch;
+    if (binarySearch.search(sortedIntegers, 1)) {
+        cout << "true ";
+    }
+    else {
+        cout << "false ";
+    }
+
+    for (int i = 0; i < sortedIntegers.size(); i++) {
+        cout << sortedIntegers.at(i) << " ";
+    }
+
+    
+
 
     return 0; 
 }
