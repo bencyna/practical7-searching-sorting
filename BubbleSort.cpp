344b2e35f8633745a1865cead2cf91224e57e318
Ben Cyna <benjamincyna@gmail.com>
2022-05-08 14:55:48 +0930

    added noswap bubble sort functionality

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 vector<int> BubbleSort::sort(std::vector<int> list) {
     int length = list.size();
+    bool noSwap = true;
     for (int i = 0; i < length; i++) {
         for (int j = 0; j < length; j++) {
             if (list.at(j) > list.at(j + 1)) {
@@ -11,7 +12,13 @@ vector<int> BubbleSort::sort(std::vector<int> list) {
                 int temp = list.at(j);
                 list.at(j) = list.at(j + 1);
                 list.at(j+1) = temp;
+                noSwap = false;
             }
+            if (noSwap) break;
         }
+        noSwap = true;
     }
 }
+
+// Complexity
+
